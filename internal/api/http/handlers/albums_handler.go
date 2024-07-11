3b8d00a152991706a10df2c12a4b15c87f331cf9
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-11 15:32:16 -0300

    create and use Album model

--- internal/api/http/handlers/albums_handler.go
+++ internal/api/http/handlers/albums_handler.go
@@ -4,14 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/models"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
+type album models.Album
 
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
