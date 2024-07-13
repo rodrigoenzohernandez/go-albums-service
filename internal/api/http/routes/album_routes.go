0faf3196b6e226dcdd0e9b0be5cf8af4c01bf8ca
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 12:42:58 -0300

    init albums handler

--- internal/api/http/routes/album_routes.go
+++ internal/api/http/routes/album_routes.go
@@ -3,12 +3,10 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rodrigoenzohernandez/web-service-gin/internal/api/http/handlers"
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/repository"
 )
 
-var albumHandler = handlers.Albums{}
-
-func RegisterAlbumRoutes(router *gin.Engine) {
+func RegisterAlbumRoutes(router *gin.Engine, repo repository.AlbumRepositoryInterface) {
+	albumHandler := handlers.NewAlbumHandler(repo)
 	router.GET("/albums", albumHandler.GetAll)
-	router.GET("/albums/:id", albumHandler.GetByID)
-	router.POST("/albums", albumHandler.Create)
 }
