e183bd0dd77399e6363f91e3506bd0ac537423a5
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 00:19:50 -0300

    implement config load and db connection on server init

--- internal/api/http/server/server.go
+++ internal/api/http/server/server.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigoenzohernandez/web-service-gin/config"
 	"github.com/rodrigoenzohernandez/web-service-gin/internal/api/http/routes"
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/database"
 	"github.com/rodrigoenzohernandez/web-service-gin/internal/utils/logger"
 )
 
 var log = logger.GetLogger("server")
 
 func InitServer() {
+	config.Load()
+	db := database.Connect()
+	defer database.Disconnect(db)
 	router := gin.Default()
 	routes.RegisterAlbumRoutes(router)
 	log.Info("Starting server on localhost:8080")
