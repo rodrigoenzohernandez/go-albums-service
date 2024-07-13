9a636e216081897b08c432da23b77cb43253135f
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 12:42:36 -0300

    update from database to repo, init album repo and pass it to regiterAlbumRoutes

--- internal/api/http/server/server.go
+++ internal/api/http/server/server.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rodrigoenzohernandez/web-service-gin/config"
 	"github.com/rodrigoenzohernandez/web-service-gin/internal/api/http/routes"
-	"github.com/rodrigoenzohernandez/web-service-gin/internal/database"
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/repository"
 	"github.com/rodrigoenzohernandez/web-service-gin/internal/utils/logger"
 )
 
@@ -12,10 +12,11 @@ var log = logger.GetLogger("server")
 
 func InitServer() {
 	config.Load()
-	db := database.Connect()
-	defer database.Disconnect(db)
+	db := repository.Connect()
+	defer repository.Disconnect(db)
+	albumRepo := repository.NewAlbumRepo(db)
 	router := gin.Default()
-	routes.RegisterAlbumRoutes(router)
+	routes.RegisterAlbumRoutes(router, albumRepo)
 	log.Info("Starting server on localhost:8080")
 	router.Run("localhost:8080")
 }
