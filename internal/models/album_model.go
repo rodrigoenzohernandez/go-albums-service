6346c8496aa6673a809ed71e8745fb6137706c05
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 13:53:13 -0300

    update the album structure so it's required

--- internal/models/album_model.go
+++ internal/models/album_model.go
@@ -2,7 +2,7 @@ package models
 
 type Album struct {
 	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Title  string  `json:"title"  binding:"required"`
+	Artist string  `json:"artist"  binding:"required"`
+	Price  float64 `json:"price"  binding:"required"`
 }
