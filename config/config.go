a19156dd01b8d40af4ade641bdceb1809370ac0f
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 00:17:36 -0300

    create config file to load the envs from the .env file and another one to get the env variable

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/utils/logger"
+
+	"github.com/joho/godotenv"
+)
+
+var log = logger.GetLogger("database")
+
+// Load the environment variables from the .env file.
+func Load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Error("Error loading .env file")
+	}
+
+}
+
+// GetEnv returns the value of an environment variable or a fallback value if it doesn't exist.
+func GetEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
