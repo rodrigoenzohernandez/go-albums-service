3aafe5585adaba2ccd407ca2285b3e8f39c77451
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-07-13 00:19:24 -0300

    create db file with Connect and Disconnect functions

---
+++ internal/database/postgresql.go
@@ -0,0 +1,50 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/rodrigoenzohernandez/web-service-gin/config"
+	"github.com/rodrigoenzohernandez/web-service-gin/internal/utils/logger"
+
+	_ "github.com/lib/pq"
+)
+
+var log = logger.GetLogger("database")
+
+func Connect() *sql.DB {
+
+	host := config.GetEnv("DB_HOST", "localhost")
+	user := config.GetEnv("DB_USER", "postgres")
+	dbName := config.GetEnv("DB_NAME", "dev")
+	password := config.GetEnv("DB_PASSWORD", "p4ssw0rd.db")
+	connectTimeout := config.GetEnv("DB_CONNECT_TIMEOUT", "5")
+
+	connStr := fmt.Sprintf("host=%s user=%s dbname=%s password=%s connect_timeout=%s",
+		host, user, dbName, password, connectTimeout)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Error("Error connecting to the database.")
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Error("Error reaching to the database.")
+		panic(err)
+
+	}
+
+	log.Info("Successfully connected to the database.")
+
+	return db
+}
+
+func Disconnect(db *sql.DB) {
+	if err := db.Close(); err != nil {
+		log.Error("Error disconnecting from the database.")
+		panic(err)
+
+	}
+}
